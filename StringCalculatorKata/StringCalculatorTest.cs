f8241effa939bdae5923bd213a10fbba61968551
Alexander Szczepanski <vampyr@beinacht.org>
2013-08-07 06:01:17 +0200

    test emptystring

--- StringCalculatorKata/StringCalculatorTest.cs
+++ StringCalculatorKata/StringCalculatorTest.cs
@@ -5,6 +5,19 @@ namespace StringCalculatorKata
     [TestFixture]
     public class StringCalculatorTest
     {
+        [Test]
+        public void Add_EmptyString_ReturnZero()
+        {
+            int result = StringCalculator.Add("");
+            Assert.AreEqual(result, 0);
+        }
+    }
 
+    public class StringCalculator
+    {
+        public static int Add(string value)
+        {
+            return 0;
+        }
     }
 }
